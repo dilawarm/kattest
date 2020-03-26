0e65921be975e120cf476f02ffb2a785158fc37d
Dilawar Mahmood <dilawarm@stud.ntnu.no>
2020-03-26 22:19:02 +0100

    error messaging

--- kattest/__main__.py
+++ kattest/__main__.py
@@ -2,14 +2,11 @@ import sys
 from kattest.script import kattest
 
 def main():
-    try:
-        cmd = sys.argv[1:]
-        if cmd[0] == 'CF':
-            kattest(cmd[1], False)
-        else:
-            kattest(cmd[0], True)
-    except:
-        print("Could not run your code.")
-        
+    cmd = sys.argv[1:]
+    if cmd[0] == 'CF':
+        kattest(cmd[1], False)
+    else:
+        kattest(cmd[0], True)
+
 if __name__ == "__main__":
     main()
