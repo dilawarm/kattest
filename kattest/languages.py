d408004853b29c0fddc7ff8dd85e5ff32e4a144e
Dilawar Mahmood <dilawarm@stud.ntnu.no>
2020-03-26 15:01:13 +0100

    update for c++ and c

--- kattest/languages.py
+++ kattest/languages.py
@@ -13,7 +13,7 @@ def python(filename, inputs):
 def cpp(filename, inputs):
     output = {}
     problem = filename.split(".")[0]
-    check_output(["g++", filename, "-g", "-O2", "-std=gnu++17", "-o", f"{problem}"])
+    check_output(["g++", filename, "-g", "-O2", "-o", f"{problem}"])
     startTime = time.time()
     for input in inputs:
         output[input] = check_output([f"./{problem}"], input=input, universal_newlines=True)
@@ -24,7 +24,7 @@ def cpp(filename, inputs):
 def c(filename, inputs):
     output = {}
     problem = filename.split(".")[0]
-    check_output(["gcc", filename, "-g", "-O2", "-std=gnu11", "-o", f"{problem}"])
+    check_output(["gcc", filename, "-g", "-O2", "-o", f"{problem}"])
     startTime = time.time()
     for input in inputs:
         output[input] = check_output([f"./{problem}"], input=input, universal_newlines=True)
